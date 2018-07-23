cb76d6410508daf16a082a16a26aafc7d47267c8
Desgard_Duan <gua@desgard.com>
2018-07-23 09:20:00 +0800

    临时提交
    Signed-off-by: Desgard_Duan <gua@desgard.com>

--- SwiftGG-WeApp/pages/index/index.js
+++ SwiftGG-WeApp/pages/index/index.js
@@ -5,9 +5,8 @@ const app = getApp()
 Page({
     data: {
         motto: 'SwiftGG, 最走心的翻译组',
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo')
+        title: '这是一个标题',
+        author: '作者: 冬瓜',
     },
     //事件处理函数
     bindViewTap: function () {
@@ -17,32 +16,6 @@ Page({
     },
     onLoad: function () {
         console.log("onLoad");
-        if (app.globalData.userInfo) {
-            this.setData({
-                userInfo: app.globalData.userInfo,
-                hasUserInfo: true
-            })
-        } else if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res => {
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                })
-            }
-        } else {
-            // 在没有 open-type=getUserInfo 版本的兼容处理
-            wx.getUserInfo({
-                success: res => {
-                    app.globalData.userInfo = res.userInfo
-                    this.setData({
-                        userInfo: res.userInfo,
-                        hasUserInfo: true
-                    })
-                }
-            })
-        }
     },
     onShow: function() {
         console.log("onShow");
